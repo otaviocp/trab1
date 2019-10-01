c9709b69835a1cdccff0fa22e7a208d169c23fe9
Daniel Bernardes Pozzan <39958473+pozzandan@users.noreply.github.com>
2019-10-01 20:10:29 -0300

    Update Trab_POO.java

--- Trab_POO.java
+++ Trab_POO.java
@@ -24,11 +24,17 @@ public class Trab_POO {
             switch(opt)
             {
                 case 1:
+                    break;
                     
-                
-                
-                
-                
+                case 2:
+                    break;
+                    
+                case 3:
+                    break;
+                    
+                case 4:
+                    break;
+                       
             }
             
         }while(opt != 0);
