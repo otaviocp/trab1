1e2895b63736bbf0aa8510fa9514bf7dc85eade1
Daniel Bernardes Pozzan <39958473+pozzandan@users.noreply.github.com>
2019-10-10 10:51:54 -0300

    Add files via upload

--- Editor.java
+++ Editor.java
@@ -1,17 +1,24 @@
-package Trab_POO;
+package trabpoo;
+import java.io.IOException;
 import java.util.*;
 
-public class Editor {
+public class Editor{
     private LinkedList<String> text;
     private Stack<String> buff;
+    private StringBuilder sb;
     
     public Editor(){
         text = new LinkedList<>();
         buff = new Stack<>();
     }
     
+    
     public void insereTexto(String s){
         text.add(s);
+    }
+    
+    public void removeTexto(){
+        text.removeLast();
     }
     
     public void desfazer(){
@@ -26,16 +33,39 @@ public class Editor {
                  s = buff.pop();
                  insereTexto(s);   
         }else{
-            System.out.println("nao eh possivel refazer!");
+            System.out.println("Não é possível refazer!");
         }
     }
     
     public void exibir(){
         for(int i = 0; i<text.size();i++)
             System.out.print(text.get(i));
+        
+        System.out.println("");
     }
     
     public void clear_list(){
         buff.clear();
+    }
+    
+    public void insereC(String c) throws IOException{
+        if(c.length()>1)
+            throw new IOException("Nao e caractere!");
+        String aux = text.removeLast();
+        sb = new StringBuilder(aux);
+        sb.append(c);
+        String res = sb.toString();
+        
+        insereTexto(res);
+    }
+    
+    public void removeC(){
+        String aux = text.removeLast();
+        sb = new StringBuilder(aux);
+        sb.deleteCharAt(aux.length()-1);
+        
+        String res = sb.toString();
+        
+        insereTexto(res);
     }
 }
--- Trab_POO.java
+++ Trab_POO.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Trab_POO;
+package trabpoo;
+import java.io.IOException;
 import static java.lang.System.*;
 import java.util.Scanner;
 
 public class Trab_POO {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int opt;
         String s;
         Scanner l = new Scanner(in);
@@ -18,9 +19,11 @@ public class Trab_POO {
             out.println("1 - Inserir texto");
             out.println("2 - Desfazer");
             out.println("3 - Refazer");
-            out.println("4 - Remove caractere");
-            out.println("5 - Remove palavra");
-            out.println("6 -  Exibir texto");
+            out.println("4 - Inserir caractere");
+            out.println("5 - Remove caractere");
+            out.println("6 - Remove palavra");
+            out.println("7 -  Exibir texto");
+            out.println(">>");
             opt = l.nextInt();
             
             switch(opt)
@@ -41,18 +44,23 @@ public class Trab_POO {
                     break;
                     
                 case 4:
+                    l.nextLine();
+                    s = l.nextLine();
+                    ed.insereC(s);
                     break;
                     
                 case 5:
+                    ed.removeC();
                     break;
                     
                 case 6:
-                    ed.exibir();
+                    ed.removeTexto();
                     break;
-                       
+                    
+                case 7:
+                    ed.exibir();
+                    break;   
             }
-            
         }while(opt != 0);
     }
-
 }
